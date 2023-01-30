eb6df0fb0df68e695ddc3a7c647c49685889c19e
Ofek Arbel <ofekarcom@gmail.com>
2023-01-30 19:34:12 +0200

    Time table

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('timetable.html')
+
+app.run(debug=True)
