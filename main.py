1ff7e767ee6b34a57a0b1aa877417db956680472
Ofek Arbel <ofekarcom@gmail.com>
2023-01-30 20:03:03 +0200

    Css fixes

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from markupsafe import Markup
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('timetable.html')
+    return render_template('timetable.html', sunday=[Markup("<b>מד\"צים ז' - אופק </b></br> (17:00-18:00)"), Markup("<b>מרחב פתוח</b></br> (16:00-21:00)")])
 
 app.run(debug=True)
